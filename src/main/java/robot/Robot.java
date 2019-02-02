de8f6b000ea38b030f8099ac3838419d20e09fc6
Angel-Per <angel.perezalvar@gmail.com>
2019-02-02 00:18:23 -0500

    Removed un-needed imports

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -11,10 +11,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends TimedRobot {
 
