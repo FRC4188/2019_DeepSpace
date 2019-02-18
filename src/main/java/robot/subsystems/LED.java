480c09f963f45ecd185a3866f6c728f9e2e03735
= <lockeadams@gmail.com>
2019-02-18 15:36:46 -0500

    Fixed LED subsystem * Resolved merge errors * Fixed style (spacing, tab size)

--- src/main/java/robot/subsystems/LED.java
+++ src/main/java/robot/subsystems/LED.java
@@ -9,64 +9,60 @@ import edu.wpi.first.wpilibj.I2C.Port;
  */
 public class LED extends Subsystem {
 
-  public static final int GREEN = 0;
-  public static final int BLUE = 1;
-  public static final int YELLOW = 2;
-  public static final int RED = 3;
-  public static final int RAINBOW = 4;
-  public static final int RAINBOW_STRIPE = 5;
-  public static final int FOREST = 6;
+    public static final int GREEN = 0;
+    public static final int BLUE = 1;
+    public static final int YELLOW = 2;
+    public static final int RED = 3;
+    public static final int RAINBOW = 4;
+    public static final int RAINBOW_STRIPE = 5;
+    public static final int FOREST = 6;
 
-  public static final int SNAKE = 0;
-  public static final int SOLID = 1;
-  public static final int FADE = 2;
+    public static final int SNAKE = 0;
+    public static final int SOLID = 1;
+    public static final int FADE = 2;
 
+    public static I2C Wire = new I2C(Port.kOnboard, 8);
+
+    /** Sets color of LEDs GREEN, BLUE, YELLOW, RED, RAINBOW, RAINBOW_STRIPE, | FOREST  */
     public void setColor(int color) {
-        command = Integer.toString(color);
-        commandC = command.toCharArray();
-        commandB = new byte[commandC.length+1];
+        String command = Integer.toString(color);
+        char[] commandC = command.toCharArray();
+        byte[] commandB = new byte[commandC.length+1];
         byte[] inC = new byte[0];
 
-  /** Sets color of LEDs GREEN, BLUE, YELLOW, RED, RAINBOW, RAINBOW_STRIPE, | FOREST  */
-  public void setColor(int color) {
-    String command = Integer.toString(color);
-    char[] commandC = command.toCharArray();
-    byte[] commandB = new byte[commandC.length+1];
-    byte[] inC = new byte[0];
+        commandB[0] = (byte)'0'; // First int of sent data 0 so color is changed
+        for(int i = 0; i < commandC.length; i++) {
+            commandB[i+1] = (byte)commandC[i];
+        }
 
-    commandB[0] = (byte)'0'; // First int of sent data 0 so color is changed
-    for(int i = 0; i < commandC.length; i++){
-      commandB[i+1] = (byte)commandC[i];
+        Wire.transaction(commandB, commandB.length, inC, 0);
     }
 
+    /** Sets routine of LEDs SNAKE, SOLID, | FADE*/
     public void setRoutine(int routine) {
-        command = Integer.toString(routine);
-        commandC = command.toCharArray();
-        commandB = new byte[commandC.length+1];
+        String command = Integer.toString(routine);
+        char[] commandC = command.toCharArray();
+        byte[] commandB = new byte[commandC.length+1];
         byte[] inC = new byte[0];
 
-  /** Sets routine of LEDs SNAKE, SOLID, | FADE*/
-  public void setRoutine(int routine) {
-    String command = Integer.toString(routine);
-    char[] commandC = command.toCharArray();
-    byte[] commandB = new byte[commandC.length+1];
-    byte[] inC = new byte[0];
+        commandB[0] = (byte)'1'; // First int of sent data 1 so routine is changed
+        for(int i = 0; i < commandC.length; i++) {
+            commandB[i+1] = (byte)commandC[i];
+        }
 
-    commandB[0] = (byte)'1'; // First int of sent data 1 so routine is changed
-    for(int i = 0; i < commandC.length; i++){
-      commandB[i+1] = (byte)commandC[i];
+        Wire.transaction(commandB, commandB.length, inC, 0);
     }
 
-    public void off(){
-        commandC = new char[] {'2'};
-        commandB = new byte[commandC.length+1];
+    /** Turns off LEDs */
+    public void off() {
+        char[] commandC = new char[] {'2'};
+        byte[] commandB = new byte[commandC.length+1];
         byte[] inC = new byte[0];
 
-  /** Turns off LEDs */
-  public void off(){
-    char[] commandC = new char[] {'2'};
-    byte[] commandB = new byte[commandC.length+1];
-    byte[] inC = new byte[0];
+        for(int i = 0; i < commandC.length; i++) {
+            commandB[i] = (byte)commandC[i];
+        }
+
         Wire.transaction(commandB, commandB.length, inC, 0);
     }
 
