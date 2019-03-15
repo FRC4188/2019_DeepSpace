d296c40ec721ebbc08ec0ac085eab880d3e32c16
= <lockeadams@gmail.com>
2019-03-15 16:48:49 -0400

    Fixed FollowPath * No more notifier, uses DistanceFollower, etc. * Need to test

--- src/main/java/robot/commands/drive/FollowPath.java
+++ src/main/java/robot/commands/drive/FollowPath.java
@@ -3,7 +3,6 @@ package robot.commands.drive;
 import robot.Robot;
 import robot.subsystems.Drivetrain;
 import robot.subsystems.LimeLight;
-import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import jaci.pathfinder.Pathfinder;
@@ -32,22 +31,21 @@ public class FollowPath extends Command {
     Drivetrain drivetrain = Robot.drivetrain;
     LimeLight limelight = Robot.limelight;
 
-    DistanceFollower leftFollower, rightFollower;
-    boolean isReversed, fromFile, isFinished;
-    double initialAngle;
-    Notifier notif;
-    Waypoint[] points;
-    Path path;
+    private DistanceFollower leftFollower, rightFollower;
+    private boolean isReversed, fromFile;
+    private double initialLeftDist, initialRightDist;
+    private Waypoint[] points;
+    private Path path;
 
-    final double kP = drivetrain.kP;
-    final double kI = drivetrain.kI;
-    final double kD = drivetrain.kD;
-    final double kV = drivetrain.kV;
-    final double kA = drivetrain.kA;
-    final double DELTA_T = 0.02; // seconds
+    private final double kP = drivetrain.kP;
+    private final double kI = drivetrain.kI;
+    private final double kD = drivetrain.kD;
+    private final double kV = drivetrain.kV;
+    private final double kA = drivetrain.kA;
+    private final double DELTA_T = 0.02; // seconds
 
-    /** Follows path from given waypoints. isReversed causes the path
-     *  to be followed in reverse. */
+  /** Follows path from given waypoints. isReversed causes the path
+     * to be followed in reverse. */
     public FollowPath(Waypoint[] points, boolean isReversed) {
         requires(drivetrain);
         this.points = points;
@@ -55,11 +53,8 @@ public class FollowPath extends Command {
         this.fromFile = false;
     }
 
-    /** Follows given path. isReversed causes the path to be followed
-     *  in reverse. */
     public FollowPath(Path path, boolean isReversed) {
         requires(drivetrain);
-        setName("FollowPath: " + path.toString());
         this.isReversed = isReversed;
         this.path = path;
         if(path == Path.TO_PERPENDICULAR) this.fromFile = false;
@@ -69,26 +64,28 @@ public class FollowPath extends Command {
     @Override
     protected void initialize() {
 
-        // save initial gyro angle to make all angles relative to first
-        initialAngle = drivetrain.getGyroAngle();
+        // initial vals
+        initialLeftDist = drivetrain.getLeftPosition();
+        initialRightDist = drivetrain.getRightPosition();
 
         // create waypoints if going to perpendicular
         if(path == Path.TO_PERPENDICULAR) {
 
             // get necessary info from camera
-            double initialAngleInRad = Math.toRadians(initialAngle);
+            double currentAngle = Math.toRadians(drivetrain.getGyroAngle());
             double turnAngle = Math.toRadians(limelight.solvePerpendicular()[0]);
-            double driveDist = limelight.solvePerpendicular()[1] * 0.9;
+            double driveDist = limelight.solvePerpendicular()[1];
             double x = driveDist * Math.cos(turnAngle);
             double y = driveDist * Math.sin(turnAngle);
             double targetAngle = Math.toRadians(limelight.solvePerpendicular()[2]);
 
-            SmartDashboard.putNumber("Path X", x);
-            SmartDashboard.putNumber("Path Y", y);
+            SmartDashboard.putNumber("x drive", x);
+            SmartDashboard.putNumber("y drive", y);
+            SmartDashboard.putNumber("target angle", targetAngle);
 
             // create points
             points = new Waypoint[] {
-                new Waypoint(0, 0, 0),
+                new Waypoint(0, 0, currentAngle),
                 new Waypoint(x, y, targetAngle)
             };
 
@@ -124,48 +121,45 @@ public class FollowPath extends Command {
         leftFollower.configurePIDVA(kP, kI, kD, kV, kA);
         rightFollower.configurePIDVA(kP, kI, kD, kV, kA);
 
-        // start notifier
-        notif = new Notifier(() -> follow());
-        notif.startPeriodic(DELTA_T);
-
     }
 
-    protected void follow() {
+    @Override
+    protected void execute() {
+
+        // invert drivetrain if needed
+        if(isReversed) {
+            drivetrain.setInverted(true);
+        }
 
         // get motor setpoints
-        double l = leftFollower.calculate(drivetrain.getLeftPosition());
-        double r = rightFollower.calculate(drivetrain.getRightPosition());
+        double l = leftFollower.calculate(drivetrain.getLeftPosition() - initialLeftDist);
+        double r = rightFollower.calculate(drivetrain.getRightPosition() - initialRightDist);
 
         // turn control loop (kP from 254 presentation)
-        double gyroHeading = drivetrain.getGyroAngle() - initialAngle;
+        double gyroHeading = drivetrain.getGyroAngle();
         double desiredHeading = Pathfinder.r2d(leftFollower.getHeading());
         double angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
-        double turn = 0.4 * (1.0/80.0) * angleDifference;
-        System.out.println("angleDiff: " + angleDifference + " turn val: " + turn);
+        double turn = 0.5 * (1.0/80.0) * angleDifference;
 
         // use output
         drivetrain.tank(l + turn, r - turn, 1.0);
-
-        // determine if finished
-        isFinished = leftFollower.isFinished() && rightFollower.isFinished() && Math.abs(angleDifference) > 5.0;
-        if(isFinished) {
-            notif.stop();
-            notif.close();
-        }
 
     }
 
     @Override
     protected boolean isFinished() {
-        return isFinished;
+        return leftFollower.isFinished() && rightFollower.isFinished();
     }
 
     @Override
     protected void end() {
+        drivetrain.tank(0, 0, 0);
+        drivetrain.setInverted(false);
     }
 
     @Override
     protected void interrupted() {
+        end();
     }
 
 }
