760ffdfc01a5218f26addbbbb696f0eaeab9a42c
Xie Ramon P <2312693224@mcsdgas.com>
2019-11-08 09:45:18 -0500

    Added auto place hatch

---
+++ src/main/java/robot/commands/groups/AutoPlaceHatch.java
@@ -0,0 +1,25 @@
+package robot.commands.groups;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import robot.commands.intake.FireHatch;
+import robot.Robot;
+import robot.subsystems.LimeLight;
+import robot.utils.*;
+import robot.commands.drive.DriveToDistance;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public class AutoPlaceHatch extends CommandGroup {
+
+    LimeLight limelight = Robot.limelight;
+    double dist;
+    double tolerance = 0.2;
+
+    public AutoPlaceHatch(){
+        dist = limelight.getDistance(limelight.getPipeline().getHeight());
+        addSequential(new DriveToDistance(dist, tolerance));
+        addParallel(new FireHatch(Value.kReverse));
+        addSequential(new Wait(), 0.2);
+        addParallel(new FireHatch(Value.kOff));
+        addSequential(new DriveToDistance(-dist, tolerance*3));
+    }
+}
